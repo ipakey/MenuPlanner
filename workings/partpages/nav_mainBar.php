6adc8b78e5052fe9c6f5f0dbd250223765944aaa
ipakey <bornagainyfke@gmail.com>
2023-02-10 20:39:27 +0000

    menus initial

---
+++ workings/partpages/nav_mainBar.php
@@ -0,0 +1,46 @@
+<nav class="nav-bar">
+        <div class="nav">
+            <button class="btn-nav dkGreen">
+                <a href="index.html" >Home</a>
+            </button>
+
+
+            .
+            <button class="btn-nav green">
+                <a>Resources</a>
+                <div class="dropdown">
+                    <a href=".assets/workings/partpages/inf_schoolDinners.php" class="btn-drop">School Dinners</a>
+                    <a href=".assets/workings/partpages/inf_nhsPortionSizes.php" class="btn-drop">NHS Child portion sizes</a>
+                    <a href=".assets/workings/partpages/inf_nhsHealthyEating.php" class="btn-drop">NHS Healthy Eating</a>
+                    <a href=".assets/workings/partpages/inf_sugar.php" class="btn-drop">Sugar Cocaine for minors</a>
+                </div>
+            </button>           
+            <button class="btn-nav blue-new">
+                <a>My Event</a>
+                <div class="dropdown">
+                    <a href=".assets/workings/partpages/frm_myCamp.php" class="btn-drop">My Camp Basic Details</a>
+                    <a href=".assets/workings/partpages/frm_dietRequests.php" class="btn-drop">Dietry Requests</a>
+                    <a href=".assets/workings/partpages/frm_allegenes.php" class="btn-drop">Allegenes</a>
+                </div>
+            </button>
+            <button class="btn-nav blue-edit">
+                <a>Recipe Book</a>
+                <div class="dropdown">
+                    <a href="./assets/workings/partpages/mealNew.php" class="btn-drop">New Meal</a>
+                    <a href="./assets/workings/partpages/frm_editAMeal.php" class="btn-drop">Edit a Meal</a>
+                    <a href="./assets/workings/partpages/frm_copyEditAMeal.php" class="btn-drop">Copy & Edit a Meal</a>
+                </div>
+            </button>
+            <button class="btn-nav burg">
+                <a href=".assets/workings/partpages/reportsLandingPage.php">Reports</a>
+                <div class="dropdown">
+                    <a href="./assets/workings/partpages/shoppingList.php" class="btn-drop">Supermarket Order</a>
+                    <a href="./assets/workings/partpages/rpt_campSummary.php" class="btn-drop">Camp Summary Adult</a>
+                    <a href="./assets/workings/partpages/rpt_dailyMealPlan.php" class="btn-drop">Daily Menu Plan with distructions</a>
+                    <a href="./assets/workings/partpages/rpt_mealDistructions.php" class="btn-drop">This Meal Plan with distructions</a>
+
+                </div>
+            </button>
+        </div>
+        <div class="log-nav">Log INOUT Form</div>
+        </nav>
---
+++ workings/partpages/rpt_mealDistructions.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="description" content="SEO info">
+    <!-- <link rel="stylesheet" href="workings/style.css"> -->
+    <!-- <title>MP . ***</title> -->
+</head>
+<body>
+    <header>
+        <h2>Page Title</h2>
+        <!-- <nav> </nav> -->
+        <p>This is the report on individual meal quantities and meal instructions</p>
+    </header>
+    <main>
+       
+    </main>
+    <!-- <script src="assets/images/yfke_logo21.png"></script> -->
+</body>
+</html>
